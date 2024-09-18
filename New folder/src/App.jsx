351906db041e3acdb7c6402b40315b82d272b262
Firoz khan <110008411+feroz-05@users.noreply.github.com>
2024-09-18 12:42:42 -0700

    Added Files

---
+++ New folder/src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/main/Main"
+import Sidebar from "./components/sidebar/Sidebar"
+
+const App = () => {
+  return (
+    <><Sidebar/>
+    <Main/>
+    </>
+  )
+}
+export default App
